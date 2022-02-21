6d616fcf711ebc5033ded4d4b661aef195d091d4
Tan Thanh <55469056+TanThanh1902@users.noreply.github.com>
2022-02-21 16:21:13 +0700

    fix res download

--- src/components/Home/AboutUs.vue
+++ src/components/Home/AboutUs.vue
@@ -1,7 +1,45 @@
 <template>
   <div>
       <v-container>
-          <h1>Về chúng tôi</h1>
+          <center>
+                <h3 color="green">Về chúng tôi</h3>
+                <div class="mt-5">
+                    <p>TopCV là công ty công nghệ nhân sự (HR Tech) hàng đầu Việt Nam. Với năng lực lõi là công nghệ, đặc biệt là trí tuệ nhân tạo (AI), sứ mệnh của TopCV đặt ra cho mình là thay đổi thị trường tuyển dụng - nhân sự ngày một hiệu quả hơn. Bằng công nghệ, chúng tôi tạo ra nền tảng cho phép người lao động tạo CV, phát triển được các kỹ năng cá nhân, xây dựng hình ảnh chuyên nghiệp trong mắt nhà tuyển dụng và tiếp cận với các cơ hội việc làm phù hợp.</p>
+                </div>
+                
+          </center>
+          <div class="mt-5">
+                    <v-row>
+                        <v-col md="3">
+                            <h4>30.000+</h4>
+                            <p>Ứng viên đang bật tìm việc trung bình/thời điểm</p>
+                        </v-col>
+                        <v-col md="3">
+                            <h4>90.000+</h4>
+                            <p>Doanh nghiệp sử dụng dịch vụ</p>
+                        </v-col>
+                        <v-col md="3">
+                            <h4>120.000+</h4>
+                            <p>Nhà tuyển dụng sử dụng thường xuyên</p>
+                        </v-col>
+                        <v-col md="3">
+                            <h4>200.000+</h4>
+                            <p>Ứng viên mới mỗi tháng</p>
+                        </v-col>
+                        <v-col md="3">
+                            <h4>3.000.000+</h4>
+                            <p>Lượt ứng viên truy cập hàng tháng</p>
+                        </v-col>
+                        <v-col md="3">
+                            <h4>4.000.000+</h4>
+                            <p>Ứng viên tiềm năng</p>
+                        </v-col>
+                        <v-col md="3">
+                            <h4>60%</h4>
+                            <p>Ứng viên có trên 2 năm kinh nghiệm</p>
+                        </v-col>
+                    </v-row>
+                </div>
       </v-container>
   </div>
 </template>
