6fae73b782b4b1069b02f3c288ffb6d9c056bb79
Nguyễn Trần Phương Vy <54873020+NguyenTranPhuongVy@users.noreply.github.com>
2022-02-24 13:16:16 +0700

    Xong front-end-app

--- src/views/Tools/GrossNet.vue
+++ src/views/Tools/GrossNet.vue
@@ -279,7 +279,7 @@
 
 <script>
 export default {
-  name: "GrossNet",
+  name: 'GrossNet',
   data() {
     return {
       windowSize: {
@@ -288,258 +288,261 @@ export default {
       },
       listForm: [
         {
-          name: "Công cụ tính lương Gross sang Net / Net sang Gross chuẩn 2022",
+          name: 'Công cụ tính lương Gross sang Net / Net sang Gross chuẩn 2022',
           listContent: [
             {
               content:
-                "Áp dụng mức giảm trừ gia cảnh mới nhất 11 triệu đồng/tháng (132 triệu đồng/năm) với nguời nộp thuế và 4,4 triệu đồng/tháng với mỗi người phụ thuộc (Theo Nghị quyết số 954/2020/UBTVQH14)",
-              type: "1",
-              text: "",
-              link: "",
+                'Áp dụng mức giảm trừ gia cảnh mới nhất 11 triệu đồng/tháng (132 triệu đồng/năm) với nguời nộp thuế và 4,4 triệu đồng/tháng với mỗi người phụ thuộc (Theo Nghị quyết số 954/2020/UBTVQH14)',
+              type: '1',
+              text: '',
+              link: '',
             },
             {
               content:
-                "Áp dụng mức lương tối thiểu vùng mới nhất có hiệu lực từ ngày 1/1/2020 (Theo điều 3, Nghị định 90/2019/NĐ-CP)",
-              type: "2",
-              text: "tối thiểu vùng",
-              link: "https://www.topcv.vn/tinh-luong-gross-net#",
+                'Áp dụng mức lương tối thiểu vùng mới nhất có hiệu lực từ ngày 1/1/2020 (Theo điều 3, Nghị định 90/2019/NĐ-CP)',
+              type: '2',
+              text: 'tối thiểu vùng',
+              link: 'https://www.topcv.vn/tinh-luong-gross-net#',
             },
           ],
         },
       ],
       listTable: [
         {
-          name: "Lương cơ sở:",
-          cost: "1,490,000đ",
+          name: 'Lương cơ sở:',
+          cost: '1,490,000đ',
         },
         {
-          name: "Giảm trừ gia cảnh bản thân:",
-          cost: "11,000,000đ",
+          name: 'Giảm trừ gia cảnh bản thân:',
+          cost: '11,000,000đ',
         },
         {
-          name: "Người phụ thuộc:",
-          cost: "4,400,000đ",
+          name: 'Người phụ thuộc:',
+          cost: '4,400,000đ',
         },
       ],
       listContentForm: [
         {
-          name: "Áp dụng quy định:",
+          name: 'Áp dụng quy định:',
           radio: [
             {
-              label: "Từ 1/7/2020",
-              value: "rule_1",
-              text: "(Mới nhất)",
-              icon: "mdi-help-circle",
+              label: 'Từ 1/7/2020',
+              value: 'rule_1',
+              text: '(Mới nhất)',
+              icon: 'mdi-help-circle',
             },
             {
-              label: "Từ 1/1/2020 - 30/6/2020",
-              value: "rule_2",
-              text: "",
-              icon: "mdi-help-circle",
+              label: 'Từ 1/1/2020 - 30/6/2020',
+              value: 'rule_2',
+              text: '',
+              icon: 'mdi-help-circle',
             },
           ],
         },
       ],
       listBoxRelated: [
         {
-          name: "Bài viết liên quan",
-          icon: "mdi-text-box",
+          name: 'Bài viết liên quan',
+          icon: 'mdi-text-box',
           type: 1,
           listContent: [
             {
-              name: "Giấy tờ thủ tục hồ sơ xin việc",
-              link: "https://www.topcv.vn/giay-to-thu-tuc-ho-so-xin-viec",
+              name: 'Giấy tờ thủ tục hồ sơ xin việc',
+              link: 'https://www.topcv.vn/giay-to-thu-tuc-ho-so-xin-viec',
             },
             {
-              name: "Tải mẫu sơ yếu lý lịch chuẩn miễn phí",
-              link: "https://www.topcv.vn/tai-mau-so-yeu-ly-lich-chuan-mien-phi",
+              name: 'Tải mẫu sơ yếu lý lịch chuẩn miễn phí',
+              link: 'https://www.topcv.vn/tai-mau-so-yeu-ly-lich-chuan-mien-phi',
             },
             {
-              name: "Cách viết Email xin việc tiếng Anh",
-              link: "https://www.topcv.vn/cach-viet-email-xin-viec-bang-tieng-anh",
+              name: 'Cách viết Email xin việc tiếng Anh',
+              link: 'https://www.topcv.vn/cach-viet-email-xin-viec-bang-tieng-anh',
             },
             {
-              name: "Các mẫu CV tham khảo theo nhóm ngành",
-              link: "https://www.topcv.vn/tong-hop-cv-tham-khao-cac-nhom-nganh",
+              name: 'Các mẫu CV tham khảo theo nhóm ngành',
+              link: 'https://www.topcv.vn/tong-hop-cv-tham-khao-cac-nhom-nganh',
             },
             {
-              name: "Mẫu đơn xin nghỉ việc chuẩn",
-              link: "https://www.topcv.vn/mau-don-xin-nghi-viec",
+              name: 'Mẫu đơn xin nghỉ việc chuẩn',
+              link: 'https://www.topcv.vn/mau-don-xin-nghi-viec',
             },
             {
-              name: "Hướng dẫn viết CV tiếng Nhật",
-              link: "https://www.topcv.vn/huong-dan-viet-cv-tieng-nhat",
+              name: 'Hướng dẫn viết CV tiếng Nhật',
+              link: 'https://www.topcv.vn/huong-dan-viet-cv-tieng-nhat',
             },
             {
-              name: "Các mẫu CV tham khảo theo nhóm ngành",
-              link: "https://www.topcv.vn/tong-hop-cv-tham-khao-cac-nhom-nganh",
+              name: 'Các mẫu CV tham khảo theo nhóm ngành',
+              link: 'https://www.topcv.vn/tong-hop-cv-tham-khao-cac-nhom-nganh',
             },
           ],
         },
         {
-          name: "Hỗ trợ",
-          icon: "mdi-email",
+          name: 'Hỗ trợ',
+          icon: 'mdi-email',
           type: 2,
           listContent: [
             {
-              name: "Bạn có chia sẻ hay cần tư vấn về cách tính lương",
-              nameLink: "Gross/Net?",
-              link: "",
+              name: 'Bạn có chia sẻ hay cần tư vấn về cách tính lương',
+              nameLink: 'Gross/Net?',
+              link: '',
             },
             {
-              name: "Hãy gửi email đề xuất tới",
-              nameLink: "hotro@topcv.vn.",
-              link: "hotro@topcv.vn",
+              name: 'Hãy gửi email đề xuất tới',
+              nameLink: 'hotro@topcv.vn.',
+              link: 'hotro@topcv.vn',
             },
           ],
         },
       ],
       listPageContent: [
         {
-          name: "Lương Gross là gì? Lương Net là gì?",
-          image: "",
-          link: "",
+          name: 'Lương Gross là gì? Lương Net là gì?',
+          image: '',
+          link: '',
           listContent: [
             {
-              name: "Lương Gross",
+              name: 'Lương Gross',
               content:
-                "là tổng thu nhập của người lao động, bao gồm cả các khoản đóng bảo hiểm và thuế. Mức lương thực nhận của bạn thuờng sẽ thấp hơn mức này vì bạn phải trích ra một phần để đóng bảo hiểm và nộp thuế thu nhập cá nhân.",
+                'là tổng thu nhập của người lao động, bao gồm cả các khoản đóng bảo hiểm và thuế. Mức lương thực nhận của bạn thuờng sẽ thấp hơn mức này vì bạn phải trích ra một phần để đóng bảo hiểm và nộp thuế thu nhập cá nhân.',
             },
             {
-              name: "Lương Net",
+              name: 'Lương Net',
               content:
-                "là lương thực nhận của bạn sau khi đã trừ hết các khoản chi phí bảo hiểm và thuế thu nhập cá nhân, hiểu đơn giản nó chính là số tiền bạn có thể bỏ túi.",
+                'là lương thực nhận của bạn sau khi đã trừ hết các khoản chi phí bảo hiểm và thuế thu nhập cá nhân, hiểu đơn giản nó chính là số tiền bạn có thể bỏ túi.',
             },
             {
-              name: "Diễn giải quan hệ giữa lương Gross và lương Net bằng công thức:",
-              content: "",
+              name: 'Diễn giải quan hệ giữa lương Gross và lương Net bằng công thức:',
+              content: '',
             },
           ],
           description:
-            "LUƠNG NET = LƯƠNG GROSS - (BHXH + BHYT + BHTN + THUẾ TNCN)",
-          time: "",
-          class: "content-dash-center",
+            'LUƠNG NET = LƯƠNG GROSS - (BHXH + BHYT + BHTN + THUẾ TNCN)',
+          time: '',
+          class: 'content-dash-center',
         },
         {
-          name: "Lương Gross hay lương Net có lợi hơn cho người lao động?",
-          image: "",
-          link: "",
+          name: 'Lương Gross hay lương Net có lợi hơn cho người lao động?',
+          image: '',
+          link: '',
           listContent: [
             {
-              name: "",
+              name: '',
               content:
-                "Nếu thoạt nhìn có thể bạn cho rằng lương Net có lợi hơn vì đó chính là số tiền bạn nhận được, còn lương Gross thì bạn có cảm giác bị mất đi một khoản khi nhận lương.",
+                'Nếu thoạt nhìn có thể bạn cho rằng lương Net có lợi hơn vì đó chính là số tiền bạn nhận được, còn lương Gross thì bạn có cảm giác bị mất đi một khoản khi nhận lương.',
             },
             {
-              name: "",
+              name: '',
               content:
-                "Trên thực tế khi bạn quy đổi lương Net sang Gross hay từ Gross sang Net thì số tiền bạn nhận được không đổi, dù bạn đàm phàn với nhà tuyển dụng bằng cách nào nào thì nhà tuyển dụng sẽ tính toán để lương Net và lương Gross ở mức tương đương nhau.",
+                'Trên thực tế khi bạn quy đổi lương Net sang Gross hay từ Gross sang Net thì số tiền bạn nhận được không đổi, dù bạn đàm phàn với nhà tuyển dụng bằng cách nào nào thì nhà tuyển dụng sẽ tính toán để lương Net và lương Gross ở mức tương đương nhau.',
             },
           ],
           description:
-            "Ví dụ: Mức lương Gross là 15,000,000 VND sẽ tương đương với mức lương Net 13,303,750 VND trong trường hợp đóng bảo hiểm trên lương chính thức Vậy nên khi nhà tuyển dụng quyết định trả bạn mức lương Gross là 15,000,000 VND, nếu bạn đàm phán mức lương Net thì lương của bạn sẽ là 13,303,750 VND và ngược lại. Về cơ bản lương bạn nhận được vẫn không đổi dù bạn đàm phán lương Net hay Gross.",
-          time: "Theo quy định mới nhất (Từ 1/7/2020)",
-          class: "content-dash",
+            'Ví dụ: Mức lương Gross là 15,000,000 VND sẽ tương đương với mức lương Net 13,303,750 VND trong trường hợp đóng bảo hiểm trên lương chính thức Vậy nên khi nhà tuyển dụng quyết định trả bạn mức lương Gross là 15,000,000 VND, nếu bạn đàm phán mức lương Net thì lương của bạn sẽ là 13,303,750 VND và ngược lại. Về cơ bản lương bạn nhận được vẫn không đổi dù bạn đàm phán lương Net hay Gross.',
+          time: 'Theo quy định mới nhất (Từ 1/7/2020)',
+          class: 'content-dash',
         },
         {
-          name: "",
-          image: "",
-          link: "",
+          name: '',
+          image: '',
+          link: '',
           listContent: [
             {
-              name: "",
+              name: '',
               content:
-                "Tuy nhiên nếu tinh ý, bạn sẽ thấy rằng khi sử dụng lương Gross mức lương bạn nhận được sẽ minh bạch hơn. Với mức lương Gross bạn sẽ chủ động tính toán được mức lương Net của mình và biết được các khoản bảo hiểm, thuế mà công ty đóng cho bạn, nguợc lại với lương Net có thể bạn sẽ không biết được mức bảo hiểm công ty đóng cho mình là bao nhiêu.",
+                'Tuy nhiên nếu tinh ý, bạn sẽ thấy rằng khi sử dụng lương Gross mức lương bạn nhận được sẽ minh bạch hơn. Với mức lương Gross bạn sẽ chủ động tính toán được mức lương Net của mình và biết được các khoản bảo hiểm, thuế mà công ty đóng cho bạn, nguợc lại với lương Net có thể bạn sẽ không biết được mức bảo hiểm công ty đóng cho mình là bao nhiêu.',
             },
           ],
           description:
-            "Ví dụ: Mức lương Net là 13,500,000 VND - Trường hợp đóng bảo hiểm trên mức lương chính thức lương Gross sẽ là 15,230,815 VND - Trường hợp đóng bảo hiểm ở mức 5,000,000 VND lương Gross sẽ là 14,156,579 VND Như ví dụ trên, bạn có thể thấy cùng một mức lương Net nhưng với mức đóng bảo hiểm khác nhau mức lương Gross bạn nhận được chênh nhau khá nhiều, lương Gross sẽ thể hiện chính xác quyền lợi bạn nhận được hơn là lương Net.",
-          time: "Theo quy định mới nhất (Từ 1/7/2020)",
-          class: "content-dash",
+            'Ví dụ: Mức lương Net là 13,500,000 VND - Trường hợp đóng bảo hiểm trên mức lương chính thức lương Gross sẽ là 15,230,815 VND - Trường hợp đóng bảo hiểm ở mức 5,000,000 VND lương Gross sẽ là 14,156,579 VND Như ví dụ trên, bạn có thể thấy cùng một mức lương Net nhưng với mức đóng bảo hiểm khác nhau mức lương Gross bạn nhận được chênh nhau khá nhiều, lương Gross sẽ thể hiện chính xác quyền lợi bạn nhận được hơn là lương Net.',
+          time: 'Theo quy định mới nhất (Từ 1/7/2020)',
+          class: 'content-dash',
         },
         {
-          name: "Cách chuyển đổi lương Gross sang Net / Lương Net sang Gross",
+          name: 'Cách chuyển đổi lương Gross sang Net / Lương Net sang Gross',
           image:
-            "https://static.topcv.vn/manual/cong-cu-tinh-luong-gross-net-topcv.png",
-          link: "http://localhost:8080/tinh-luong-gross-net#",
+            'https://static.topcv.vn/manual/cong-cu-tinh-luong-gross-net-topcv.png',
+          link: 'http://localhost:8080/tinh-luong-gross-net#',
           listContent: [
             {
-              name: "",
+              name: '',
               content:
-                "Cách đơn giản nhất là các bạn có thể sử dụng công cụ chuyển đổi lương Net sang Gross và Gross sang Net của TopCV. Bạn chỉ cần cung cấp các thông số: Lương, mức đóng bảo hiểm, vùng và số người phụ thuộc, công cụ sẽ tính toán và phân tích diễn giải từng loại chi phí để bạn hiểu rõ nhất mức lương của mình.",
+                'Cách đơn giản nhất là các bạn có thể sử dụng công cụ chuyển đổi lương Net sang Gross và Gross sang Net của TopCV. Bạn chỉ cần cung cấp các thông số: Lương, mức đóng bảo hiểm, vùng và số người phụ thuộc, công cụ sẽ tính toán và phân tích diễn giải từng loại chi phí để bạn hiểu rõ nhất mức lương của mình.',
             },
           ],
-          description: "",
-          time: "",
-          class: "",
+          description: '',
+          time: '',
+          class: '',
         },
       ],
       listLicenseTable: [
         {
-          name: "Bản quyền nội dung thuộc về TopCV.vn, được bảo vệ bởi Luật bảo vệ bản quyền tác giả",
-          link: "https://www.dmca.com/Protection/Status.aspx?ID=1b16a667-a95e-4730-846f-46f962522fce",
-          text: "DMCA.",
+          name: 'Bản quyền nội dung thuộc về TopCV.vn, được bảo vệ bởi Luật bảo vệ bản quyền tác giả',
+          link: 'https://www.dmca.com/Protection/Status.aspx?ID=1b16a667-a95e-4730-846f-46f962522fce',
+          text: 'DMCA.',
         },
         {
-          name: "Vui lòng không trích dẫn nội dung trang web khi chưa được sự cho phép của TopCV.",
-          link: "",
-          text: "",
+          name: 'Vui lòng không trích dẫn nội dung trang web khi chưa được sự cho phép của TopCV.',
+          link: '',
+          text: '',
         },
       ],
       radioWage: [
         {
-          label: "I",
-          value: "wage-1",
+          label: 'I',
+          value: 'wage-1',
         },
         {
-          label: "II",
-          value: "wage-2",
+          label: 'II',
+          value: 'wage-2',
         },
         {
-          label: "III",
-          value: "wage-3",
+          label: 'III',
+          value: 'wage-3',
         },
         {
-          label: "IV",
-          value: "wage-4",
+          label: 'IV',
+          value: 'wage-4',
         },
       ],
-      row_rule: "rule_1",
-      row_insurance: "insurance_1",
-      row_line: "wage-1",
-    };
+      row_rule: 'rule_1',
+      row_insurance: 'insurance_1',
+      row_line: 'wage-1',
+    }
   },
   mounted() {
-    this.onResize();
+    this.onResize()
   },
 
   methods: {
     onResize() {
-      this.windowSize = { x: window.innerWidth, y: window.innerHeight };
+      this.windowSize = { x: window.innerWidth, y: window.innerHeight }
       if (this.windowSize.x < 600) {
         this.common.image = {
-          width: "75%",
-          height: "75%",
-        };
+          width: '75%',
+          height: '75%',
+        }
       } else if (this.windowSize.x < 960) {
         this.common.image = {
-          width: "90%",
-          height: "90%",
-        };
+          width: '90%',
+          height: '90%',
+        }
       } else {
         this.common.image = {
-          width: "100%",
-          height: "100%",
-        };
+          width: '100%',
+          height: '100%',
+        }
       }
     },
   },
-};
+}
 </script>
 
 <style>
 .grossnet-font label {
   font-size: 14px;
+}
+.grossnet-font b {
+  font-size: 16px;
 }
 .radio-style {
   padding-top: 0 !important;
