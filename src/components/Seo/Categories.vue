3ab439c5a3af344ab9d13216b95439a45fb99d13
Tan Thanh <55469056+TanThanh1902@users.noreply.github.com>
2022-02-26 20:08:11 +0700

    done
    complete gift, content and category

--- src/components/Seo/Categories.vue
+++ src/components/Seo/Categories.vue
@@ -1,13 +1,277 @@
 <template>
-    <h1>Categories</h1>
+  <div>
+      <b>Từ khoá tìm việc làm phổ biến tại TopCV</b>
+    <v-row class="mt-5">
+      <v-col cols="12" sm="6" md="4">
+        <div style="background: #f2fbf6 0 0 no-repeat padding-box">
+          <v-card outlined class="p-2" style="background: #f2fbf6 0 0 no-repeat padding-box"><b>Tìm việc làm theo ngành nghề</b> </v-card>
+          <v-card
+            v-scroll.self="onScroll"
+            class="overflow-y-auto"
+            max-height="300"
+          >
+            <v-card style="background: #f2fbf6 0 0 no-repeat padding-box" v-for="(item, i) in workforCareer" :key="i" tile outlined>
+              <a class="p-2" style="text-decoration: none; color: black">{{ item.title }}</a>
+            </v-card>
+          </v-card>
+        </div>
+      </v-col>
+
+      <v-col cols="12" sm="6" md="4">
+        <div>
+          <v-card style="background: #f2fbf6 0 0 no-repeat padding-box" outlined class="p-2"><b>Tìm việc làm tại nhà</b> </v-card>
+          <v-card
+            v-scroll.self="onScroll"
+            class="overflow-y-auto"
+            max-height="300"
+          >
+            <v-card style="background: #f2fbf6 0 0 no-repeat padding-box" v-for="(item, i) in workFromHome" :key="i" tile outlined>
+              <a class="p-2" style="text-decoration: none; color: black">{{ item.title }}</a>
+            </v-card>
+          </v-card>
+        </div>
+      </v-col>
+
+      <v-col cols="12" sm="6" md="4">
+        <div>
+          <v-card style="background: #f2fbf6 0 0 no-repeat padding-box" outlined class="p-2"><b>Việc làm phổ biến</b> </v-card>
+          <v-card
+            v-scroll.self="onScroll"
+            class="overflow-y-auto"
+            max-height="300"
+          >
+            <v-card style="background: #f2fbf6 0 0 no-repeat padding-box" v-for="(item, i) in popularWork" :key="i" tile outlined>
+              <a class="p-2" style="text-decoration: none; color: black">{{ item.title }}</a>
+            </v-card>
+          </v-card>
+        </div>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Categories'
-}
+  name: "Categories",
+  data() {
+    return {
+      workforCareer: [
+        {
+          title: "Việc làm An toàn lao động",
+          link: "",
+        },
+        {
+          title: "Việc làm Bán hàng kỹ thuật",
+          link: "",
+        },
+        {
+          title: "Việc làm Bán lẻ/ bán sỉ",
+          link: "",
+        },
+        {
+          title: "Việc làm Báo chí / Rruyền hình",
+          link: "",
+        },
+        {
+          title: "Việc làm Bảo hiểm",
+          link: "",
+        },
+        {
+          title: "Việc làm Bảo trì / Sửa chữa",
+          link: "",
+        },
+        {
+          title: "Việc làm Bất động sản",
+          link: "",
+        },
+        {
+          title: "Việc làm Bưu chính viễn thông",
+          link: "",
+        },
+        {
+          title: "Việc làm An toàn lao động",
+          link: "",
+        },
+        {
+          title: "Việc làm Bán hàng kỹ thuật",
+          link: "",
+        },
+        {
+          title: "Việc làm Bán lẻ/ bán sỉ",
+          link: "",
+        },
+        {
+          title: "Việc làm Báo chí / Rruyền hình",
+          link: "",
+        },
+        {
+          title: "Việc làm Bảo hiểm",
+          link: "",
+        },
+        {
+          title: "Việc làm Bảo trì / Sửa chữa",
+          link: "",
+        },
+        {
+          title: "Việc làm Bất động sản",
+          link: "",
+        },
+        {
+          title: "Việc làm Bưu chính viễn thông",
+          link: "",
+        },
+      ],
+      workFromHome: [
+        {
+          title: "Hà Nội",
+          link: "",
+        },
+        {
+          title: "TP Hồ Chí Minh",
+          link: "",
+        },
+        {
+          title: "Đà Nẵng",
+          link: "",
+        },
+        {
+          title: "Bình Dương",
+          link: "",
+        },
+        {
+          title: "Đồng Nai",
+          link: "",
+        },
+        {
+          title: "Tây Ninh",
+          link: "",
+        },
+        {
+          title: "Sóc Trăng",
+          link: "",
+        },
+        {
+          title: "Bình Thuận",
+          link: "",
+        },
+        {
+          title: "Quảng Ninh",
+          link: "",
+        },
+        {
+          title: "Quảng Nam",
+          link: "",
+        },
+        {
+          title: "Hà Nội",
+          link: "",
+        },
+        {
+          title: "TP Hồ Chí Minh",
+          link: "",
+        },
+        {
+          title: "Đà Nẵng",
+          link: "",
+        },
+        {
+          title: "Bình Dương",
+          link: "",
+        },
+        {
+          title: "Đồng Nai",
+          link: "",
+        },
+        {
+          title: "Tây Ninh",
+          link: "",
+        },
+        {
+          title: "Sóc Trăng",
+          link: "",
+        },
+        {
+          title: "Bình Thuận",
+          link: "",
+        },
+        {
+          title: "Quảng Ninh",
+          link: "",
+        },
+        {
+          title: "Quảng Nam",
+          link: "",
+        },
+      ],
+      popularWork: [
+        {
+          title: "Tìm việc làm Thực tập sinh",
+          link: "",
+        },
+        {
+          title: "Tìm việc làm Trợ giảng",
+          link: "",
+        },
+        {
+          title: "Tìm việc làm Giáo viên",
+          link: "",
+        },
+        {
+          title: "Tìm việc làm Thư ký",
+          link: "",
+        },
+        {
+          title: "Tìm việc làm Nhân viên kỹ thuật",
+          link: "",
+        },
+        {
+          title: "Tìm việc làm IT Helpdesk",
+          link: "",
+        },
+        {
+          title: "Tìm việc làm Lập trình viên PHP",
+          link: "",
+        },
+        {
+          title: "Tìm việc làm Lập trình viên Javascript",
+          link: "",
+        },
+        {
+          title: "Tìm việc làm Lập trình viên ReactJS",
+          link: "",
+        },
+        {
+          title: "Tìm việc làm Lập trình viên VueJS",
+          link: "",
+        },
+        {
+          title: "Tìm việc làm Lập trình viên AngularJS",
+          link: "",
+        },
+        {
+          title: "Tìm việc làm Lập trình viên NodeJS",
+          link: "",
+        },
+        {
+          title: "Tìm việc làm Lập trình viên Java",
+          link: "",
+        },
+        {
+          title: "Tìm việc làm Lập trình viên .NET",
+          link: "",
+        },
+        {
+          title: "Tìm việc làm Lập trình Front-End",
+          link: "",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-
+.v-card .a:hover{
+    color: #2db14f
+}
 </style>
--- src/components/Seo/Content.vue
+++ src/components/Seo/Content.vue
@@ -1,10 +1,19 @@
 <template>
-  <h1>Content</h1>
+  <div>
+    <p>
+      {{content}}
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Content'
+    name: 'Content',
+    data(){
+      return{
+        content:"Cơ hội ứng tuyển việc làm với đãi ngộ hấp dẫn tại các công ty hàng đầu Trước sự phát triển vượt bậc của nền kinh tế, rất nhiều ngành nghề trở nên khan hiếm nhân lực hoặc thiếu nhân lực giỏi. Vì vậy, hầu hết các trường Đại học đều liên kết với các công ty, doanh nghiệp, cơ quan để tạo cơ hội cho các bạn sinh viên được học tập, rèn luyện bản thân và làm quen với môi trường làm việc từ sớm. Trong danh sách việc làm trên đây, TopCV mang đến cho bạn những cơ hội việc làm tại những môi trường làm việc năng động, chuyên nghiệp. Vậy tại sao nên tìm việc làm tại TopCV? Việc làm Chất lượng Hàng ngàn tin tuyển dụng chất lượng cao được cập nhật thường xuyên để đáp ứng nhu cầu tìm việc của ứng viên. Hệ thống thông minh tự động gợi ý các công việc phù hợp theo CV của bạn. Công cụ viết CV đẹp Miễn phí Nhiều mẫu CV đẹp, phù hợp nhu cầu ứng tuyển các vị trí khác nhau. Tương tác trực quan, dễ dàng chỉnh sửa thông tin, tạo CV online nhanh chóng trong vòng 5 phút. Hỗ trợ Người tìm việc Nhà tuyển dụng chủ động tìm kiếm và liên hệ với bạn qua hệ thống kết nối ứng viên thông minh. Báo cáo chi tiết Nhà tuyển dụng đã xem CV và gửi offer tới bạn. Bảo mật & An toàn tuyệt đối Bạn có thể chủ động bật / tắt trạng thái tìm việc, trạng thái cho phép Nhà tuyển dụng xem hồ sơ. Nếu các trạng thái tắt, không ai có thể xem được CV của bạn. Các Nhà tuyển dụng đều được TopCV xác thực rõ ràng danh tính, đảm bảo đến từ các công ty uy tín, giúp bạn yên tâm hơn khi ứng tuyển và sớm chủ động nhận được phản hổi. Tại TopCV, bạn có thể tìm thấy những tin tuyển dụng việc làm với mức lương vô cùng hấp dẫn. Những nhà tuyển dụng kết nối với TopCV đều là những công ty lớn tại Việt Nam, nơi bạn có thể làm việc trong một môi trường chuyên nghiệp, năng động, trẻ trung. TopCV là nền tảng tuyển dụng công nghệ cao giúp các nhà tuyển dụng và ứng viên kết nối với nhau. Nhanh tay tạo CV để ứng tuyển vào các vị trí việc làm mới nhất hấp dẫn tại việc làm mới nhất tại Hà Nội, việc làm mới nhất tại TP.HCM ở TopCV, bạn sẽ tìm thấy những việc làm mới nhất với mức lương tốt nhất!"
+      }
+    }
 }
 </script>
 
